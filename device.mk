af130f509554bb2c966dab59d84eeb54b534be03
Weilun Du <wdu@google.com>
2020-08-21 14:45:14 -0700

    mt6768-common: rro_overlays: Add TetheringOverlay
    BUG: 163800715
    Signed-off-by: Weilun Du <wdu@google.com>

--- device.mk
+++ device.mk
@@ -40,6 +40,9 @@ PRODUCT_PACKAGES += \
     android.hardware.health@2.1-impl.recovery \
     android.hardware.health@2.1-service
 
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay
+
 # Overlays
 PRODUCT_ENFORCE_RRO_TARGETS := *
 
