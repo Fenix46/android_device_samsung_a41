3c21b4186c980e2fc25795efa769eedeeeadf83b
Emanuele Scarlata <scarlataemanuele@gmail.com>
2023-07-17 18:28:01 +0200

    Initial Device Tree for Galaxy A41 from A415FXXU3DWD2
    Signed-off-by: Emanuele Scarlata <scarlataemanuele@gmail.com>

---
+++ AndroidProducts.mk
@@ -0,0 +1,13 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+PRODUCT_MAKEFILES := \
+    $(LOCAL_DIR)/aosp_a41.mk
+
+COMMON_LUNCH_CHOICES := \
+    aosp_a41-user \
+    aosp_a41-userdebug \
+    aosp_a41-eng
---
+++ device.mk
@@ -0,0 +1,75 @@
+#
+# Copyright (C) 2023 The LineageOS Project
+#
+# SPDX-License-Identifier: Apache-2.0
+#
+
+# Enable updating of APEXes
+$(call inherit-product, $(SRC_TARGET_DIR)/product/updatable_apex.mk)
+
+# Include GSI keys
+$(call inherit-product, $(SRC_TARGET_DIR)/product/gsi_keys.mk)
+
+# fastbootd
+PRODUCT_PACKAGES += \
+    android.hardware.fastboot@1.1-impl-mock \
+    fastbootd
+
+# Health
+PRODUCT_PACKAGES += \
+    android.hardware.health@2.1-impl \
+    android.hardware.health@2.1-impl.recovery \
+    android.hardware.health@2.1-service
+
+# Overlays
+PRODUCT_ENFORCE_RRO_TARGETS := *
+
+# Partitions
+PRODUCT_BUILD_SUPER_PARTITION := false
+PRODUCT_USE_DYNAMIC_PARTITIONS := true
+
+# Product characteristics
+PRODUCT_CHARACTERISTICS := phone
+
+# Rootdir
+PRODUCT_PACKAGES += \
+    install-recovery.sh \
+    init.insmod.sh \
+
+PRODUCT_PACKAGES += \
+    fstab.enableswap \
+    factory_init.project.rc \
+    factory_init.connectivity.common.rc \
+    meta_init.modem.rc \
+    init.ago.rc \
+    init.connectivity.common.rc \
+    factory_init.connectivity.rc \
+    meta_init.rc \
+    factory_init.rc \
+    init.project.rc \
+    init.cgroup.rc \
+    init.modem.rc \
+    init.mt6768.usb.rc \
+    init_connectivity.rc \
+    init.aee.rc \
+    multi_init.rc \
+    meta_init.project.rc \
+    meta_init.connectivity.rc \
+    meta_init.connectivity.common.rc \
+    init.mt6768.rc \
+    init.connectivity.rc \
+    init.recovery.mt6768.rc \
+    init.recovery.samsung.rc \
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/rootdir/etc/fstab.enableswap:$(TARGET_COPY_OUT_RAMDISK)/fstab.enableswap
+
+# Shipping API level
+PRODUCT_SHIPPING_API_LEVEL := 29
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# Inherit the proprietary files
+$(call inherit-product, vendor/samsung/a41/a41-vendor.mk)
